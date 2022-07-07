bee1d57c1e17f49b0ee0ba6aa46be624ac4d4044
Zsolt Ienei <J_Zsolt>
2022-07-07 16:41:41 +0300

    auth page sceleton

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  exports: [MatFormFieldModule, MatSelectModule],
+  exports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule,
+    MatGridListModule,
+    MatTabsModule,
+    MatButtonModule,
+  ],
 })
 export class MaterialModule {}
